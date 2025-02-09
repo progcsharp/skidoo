13b6c86bae6b68ebb9d226cc45560646684baa1f
unknown <fonror@mail.ru>
2025-02-09 03:20:09 +0300

    fix state clear

--- handler/message.py
+++ handler/message.py
@@ -6,5 +6,5 @@ from config import bot, ADMIN_ID
 
 async def message_form(message: types.Message, state: FSMContext):
     await bot.send_message(ADMIN_ID, f"Сообщение от пользователя: @{message.from_user.username}\nСообщение: {message.text}")
-
+    await state.clear()
     await message.answer("Спасибо за ваше сообщение! Мы рассмотрим вашу проблему.")
